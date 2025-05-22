06a207175eca1896b4eb4f3ff78b088b151d4aea
Osamaas04 <alasmarosama7@gmail.com>
2025-05-22 17:02:02 +0300

    refine 1

---
+++ model/message-model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const STATUS = {
+  ESCALATED: 0,
+  ASSIGNED: 1,
+  RESOLVED: 2,
+}
+
+const PLATFORM = {
+  FACEBOOK: "F",
+  INSTAGRAM: "I",
+  WHATSAPP: "W",
+}
+
+const messageSchema = new mongoose.Schema({
+  senderId: { type: String, required: true },
+  recipientId: { type: String, required: true },
+  messageId: { type: String, required: true },
+  text: { type: String },
+  pageAccessToken: { type: String, required: true },
+  status: {
+    type: Number,
+    enum: Object.values(STATUS),
+    default: STATUS.ESCALATED,
+  },
+  createdAt: { type: Date, default: Date.now },
+  sentAt: { type: Date, required: true },
+  platform: {
+    type: String,
+    enum: Object.values(PLATFORM),
+    required: true,
+  },
+  userId: { type: String, required: true },
+});
+
+export const Message = mongoose.models.Message || mongoose.model("Message", messageSchema);
+export const MessageStatus = STATUS;
+export const MessagePlatform = PLATFORM;
